312168c5029849f03327a1bfeb604d3a8da37773
Ziwei Zhang <44096307+zz112duke@users.noreply.github.com>
2020-12-24 13:19:31 +0800

    Add files via upload

--- exp_script.js
+++ exp_script.js
@@ -53,17 +53,17 @@ var consent = {
 timeline.push(consent);
 
 
-var page_1_options = ["G", "F", "H", "C", "X"];
-var page_2_options = ["C and X", "C and H", "X and H", "F and G"];
+//var page_1_options = ["G", "F", "H", "C", "X"];
+//var page_2_options = ["C and X", "C and H", "X and H", "F and G"];
 
-var multi_choice_block = {
-    type: 'survey-multi-choice',
-    questions: [
-        { prompt: "What was the key response for shapes with gray background?", name: 'P_Inclusion_1', options: page_1_options, required: true },
-        { prompt: "What were the two possible key responses for shapes with green or blue background?", name: 'P_Inclusion_2', options: page_2_options, required: false }
-    ],
-};
-timeline.push(multi_choice_block);
+//var multi_choice_block = {
+//    type: 'survey-multi-choice',
+//    questions: [
+//        { prompt: "What was the key response for shapes with gray background?", name: 'P_Inclusion_1', options: page_1_options, required: true },
+//        { prompt: "What were the two possible key responses for shapes with green or blue background?", name: 'P_Inclusion_2', options: page_2_options, required: false }
+//    ],
+//};
+//timeline.push(multi_choice_block);
 
 
 var instr_1 = {

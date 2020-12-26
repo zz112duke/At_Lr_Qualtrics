55de6f46d2cdf358825efc3386f861f2512b75f2
Ziwei Zhang <44096307+zz112duke@users.noreply.github.com>
2020-12-27 03:29:28 +0800

    Add files via upload

--- js/jspsych-6.1.0/plugins/jspsych-fullscreen.js
+++ js/jspsych-6.1.0/plugins/jspsych-fullscreen.js
@@ -30,7 +30,7 @@ jsPsych.plugins.fullscreen = (function() {
       button_label: {
         type: jsPsych.plugins.parameterType.STRING,
         pretty_name: 'Button label',
-        default:  'Continue',
+        default:  'Next',
         array: false,
         description: 'The text that appears on the button to enter fullscreen.'
       },
--- js/jspsych-6.1.0/plugins/jspsych-survey-multi-choice.js
+++ js/jspsych-6.1.0/plugins/jspsych-survey-multi-choice.js
@@ -69,7 +69,7 @@ jsPsych.plugins['survey-multi-choice'] = (function() {
       button_label: {
         type: jsPsych.plugins.parameterType.STRING,
         pretty_name: 'Button label',
-        default:  'Next',
+        default:  'Continue',
         description: 'Label of the button.'
       }
     }
